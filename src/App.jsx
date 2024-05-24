c10d1498621831898903bd86069fcec04ae1ac9c
saeidpourrajabi <pourrajabisaeid@gmail.com>
2024-05-24 16:56:48 +0330

    enjoy with react

--- src/App.jsx
+++ src/App.jsx
@@ -21,36 +21,38 @@ const tabData = [
 
 function App() {
   const [activeTab, setActiveTab] = useState(1);
-  const [isOpen,setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handleActiveTab = (id) => {
     setActiveTab(id);
   };
 
-  const handleOpen= ()=>{
-    setIsOpen(!isOpen)
-  }
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
-      <button onClick={()=>handleOpen()}>✖️</button>
+      <button onClick={() => handleOpen()}>✖️</button>
       {isOpen ? (
-      <div className="tab">
-      <div className="tab-header">
-        {tabData.map((tab) => (
-          <button
-            onClick={() => handleActiveTab(tab.id)}
-            key={tab.id}
-            className={activeTab === tab.id ? "active" : ""}>
-            {tab.title}
-          </button>
-        ))}
-      </div>
-      <div className="tab-content">
-        <p>{tabData[activeTab - 1].content}</p>
-      </div>
-    </div>) :(<p>closed.</p>
-  )}
+        <div className="tab">
+          <div className="tab-header">
+            {tabData.map((tab) => (
+              <button
+                onClick={() => handleActiveTab(tab.id)}
+                key={tab.id}
+                className={activeTab === tab.id ? "active" : ""}>
+                {tab.title}
+              </button>
+            ))}
+          </div>
+          <div className="tab-content">
+            <p>{tabData[activeTab - 1].content}</p>
+          </div>
+        </div>
+      ) : (
+        <p>closed.</p>
+      )}
     </div>
   );
 }
